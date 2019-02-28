20babec50b619f8ee09dbcdedcea1dbf33d45095
marcoandre1 <marco-andre.echeverria.1@ulaval.ca>
2019-02-28 06:16:19 -0500

    Updates

---
+++ precache-manifest.a8d55789b07195fdd64af2be625b5b67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4442f0588d937dab15b0",
+    "url": "/static/js/main.4442f058.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "4442f0588d937dab15b0",
+    "url": "/static/css/main.99cd15a0.chunk.css"
+  },
+  {
+    "revision": "f1ac2a27e5c0a5c1a2d7d2aac85e798c",
+    "url": "/index.html"
+  }
+];

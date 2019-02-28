86a97af35207568c3ea14b02375b8483b7425f7c
marcoandre1 <marco-andre.echeverria.1@ulaval.ca>
2019-02-28 04:20:08 -0500

    trying to deploy a folder

---
+++ build/precache-manifest.29581692ba17b5ef2d6ea4e2f6a23518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "05eff99e2468b1d35c27",
+    "url": "/static/js/main.05eff99e.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "05eff99e2468b1d35c27",
+    "url": "/static/css/main.4b8bffba.chunk.css"
+  },
+  {
+    "revision": "3ed5cfeed618a6b6c77b1358d9b1bbc0",
+    "url": "/index.html"
+  }
+];

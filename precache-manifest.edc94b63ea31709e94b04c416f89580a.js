4b602e0eee322ca424ef93f5e4b3c042ac5d2bf2
marcoandre1 <marco-andre.echeverria.1@ulaval.ca>
2020-09-19 01:46:32 -0400

    Updates

---
+++ precache-manifest.edc94b63ea31709e94b04c416f89580a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf94d214bbafa566fdcfad9daa3aa45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6384eea1d2178d3ad2ab",
+    "url": "/static/css/main.43b24836.chunk.css"
+  },
+  {
+    "revision": "96200330b6071f15afc0",
+    "url": "/static/js/2.8e82d212.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8e82d212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6384eea1d2178d3ad2ab",
+    "url": "/static/js/main.64eb98b0.chunk.js"
+  },
+  {
+    "revision": "26ec76aa0143b8f133dc",
+    "url": "/static/js/runtime-main.3963b474.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);

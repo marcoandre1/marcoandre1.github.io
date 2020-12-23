ddc84baec627f00924805596f1023856b6e12186
marcoandre1 <marco-andre.echeverria.1@ulaval.ca>
2020-12-23 09:28:15 -0500

    Add styling to navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,11 +3,22 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Navigation = () => (
-  <div className="header">
-    <Link to={`/dashboard`}>
-      <h1>Daily Organizer</h1>
-    </Link>
-  </div>
+  <nav className="flex items-center justify-between flex-wrap bg-gray-800 mb-6 p-6 rounded-md">
+    <div className="w-full block flex-grow">
+      <div className="md:flex-grow">
+        <Link to={`/`}>
+          <a className="block md:inline-block text-gray-300 hover:text-white mr-4">
+            Home
+          </a>
+        </Link>
+        <Link to={`/dashboard`}>
+          <a className="block md:inline-block text-gray-300 hover:text-white mr-4">
+            Speakers
+          </a>
+        </Link>
+      </div>
+    </div>
+  </nav>
 );
 
 export const ConnectedNavigation = connect((state) => state)(Navigation);

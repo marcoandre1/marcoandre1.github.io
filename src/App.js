f229977ee625e8dcf6f6c488751322318b272ee6
marcoandre1 <marco-andre.echeverria.1@ulaval.ca>
2020-12-28 16:51:49 -0500

    Update set dark mode method

--- src/App.js
+++ src/App.js
@@ -6,30 +6,33 @@ import { ConnectedDashboard } from "./components/Dashboard";
 import { ConnectedNavigation } from "./components/Navigation";
 import { ConnectedFooter } from "./components/Footer";
 
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC
-if (
-  localStorage.theme === "dark" ||
-  (!("theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-  document.querySelector("html").classList.add("dark");
-} else {
-  document.querySelector("html").classList.remove("dark");
-}
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      isDarkMode:
-        localStorage.theme === "dark" ||
-        (!("theme" in localStorage) &&
-          window.matchMedia("(prefers-color-scheme: dark)").matches),
+      isDarkMode: false,
       isMenuOpen: false,
     };
     // Binding method
     this.onDarkModeToggle = this.onDarkModeToggle.bind(this);
     this.onMobileMenuClick = this.onMobileMenuClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.setInitialDarkMode();
+  }
+
+  setInitialDarkMode() {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.querySelector("html").classList.add("dark");
+    } else {
+      document.querySelector("html").classList.remove("dark");
+    }
+    this.setDarkMode();
   }
 
   onMobileMenuClick() {

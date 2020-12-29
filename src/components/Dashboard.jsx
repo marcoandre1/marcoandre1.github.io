85273a75ec3eff85e526dac21dfa22d09756826a
marcoandre1 <marco-andre.echeverria.1@ulaval.ca>
2020-12-29 09:01:30 -0500

    Add Page not found component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,36 +2,49 @@ import React from "react";
 import { connect } from "react-redux";
 import { ConnectedTaskList } from "./TaskList";
 import { Helmet } from "react-helmet";
+import PageNotFound from "./PageNotFound";
 
-export const Dashboard = ({ language }) => (
+export const Dashboard = ({ language, isValidRequest }) => (
   <div>
     <Helmet>
       <title>{language.htmlTitle}</title>
     </Helmet>
-    <h2 className="font-bold text-xl text-gray-900 dark:text-white mb-2 mt-2 px-6">
-      {language.title}
-    </h2>
-    <ConnectedTaskList
-      key={language.id}
-      id={language.id}
-      name={language.name}
-      description={language.description}
-    />
+    {isValidRequest && (
+      <div>
+        <h2 className="font-bold text-xl text-gray-900 dark:text-white mb-2 mt-2 px-6">
+          {language.title}
+        </h2>
+        <ConnectedTaskList
+          key={language.id}
+          id={language.id}
+          name={language.name}
+          description={language.description}
+        />
+      </div>
+    )}
+    {!isValidRequest && (
+      <div>
+        <PageNotFound />
+      </div>
+    )}
   </div>
 );
 
 function mapStateToProps(state, ownProps) {
   let id = ownProps.match === undefined ? "en" : ownProps.match.params.id;
   let language = state.languages.find((language) => language.id === id);
+  let isValidRequest = true;
 
   if (language === undefined) {
-    id = 404;
-    language = state.pageNotFound.find((notFound) => notFound.id === id);
+    // id contains the /asdf
+    language = state.pageNotFound.find((notFound) => notFound.id === 404);
+    isValidRequest = false;
   }
 
   return {
     id,
     language,
+    isValidRequest,
   };
 }
 
---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function PageNotFound() {
+  return (
+    <div className="flex justify-center">
+      <div class="h-96 my-6 flex flex-wrap content-center">
+        <div className="component-highlight max-w-sm rounded overflow-hidden shadow-lg items-center align-middle">
+          <h2 className="font-bold text-xl text-gray-900 dark:text-white mb-2 mt-2 px-6">
+            404 | Page not found
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+}

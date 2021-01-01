24bce7381b5953b17974bb62288a6a4a3ae4198f
marcoandre1 <marco-andre.echeverria.1@ulaval.ca>
2021-01-01 00:42:39 -0500

    Add dump image

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ProfileImage from "./ProfileImage";
+import profile from "../images/profile.png";
 
 export default function Project({ id, bio, firstName, lastName }) {
   return (
@@ -8,7 +9,7 @@ export default function Project({ id, bio, firstName, lastName }) {
         <div className="font-bold text-lg col-span-3">{`${firstName} ${lastName}`}</div>
       </div>
       <div className="mb-6">
-        <ProfileImage id={id} />
+        <ProfileImage id={profile} />
       </div>
       <div className="text-gray-600">{bio.substr(0, 70) + "..."}</div>
     </div>
--- src/server/defaultState.js
+++ src/server/defaultState.js
@@ -1,74 +1,67 @@
 export const defaultState = {
-  users: [
-    {
-      id: "U1",
-      name: "Dev",
-    },
-    {
-      id: "U2",
-      name: "C. Eeyo",
-    },
-  ],
-  groups: [
-    {
-      name: "To Do",
-      id: "G1",
-      owner: "U1",
-    },
-    {
-      name: "Doing",
-      id: "G2",
-      owner: "U1",
-    },
-    {
-      name: "Done",
-      id: "G3",
-      owner: "U1",
-    },
-  ],
-  tasks: [
+  projects: [
     {
       name: "Refactor tests",
-      id: "T1",
-      group: "G1",
+      id: "P1",
+      language: "en",
       owner: "U1",
       isComplete: false,
     },
     {
       name: "Meet with CTO",
-      id: "T2",
-      group: "G1",
+      id: "P2",
+      language: "en",
       owner: "U1",
       isComplete: true,
     },
     {
       name: "Compile ES6",
-      id: "T3",
-      group: "G2",
+      id: "P3",
+      language: "en",
       owner: "U2",
       isComplete: false,
     },
     {
       name: "Update component snapshots",
-      id: "T4",
-      group: "G2",
+      id: "P4",
+      language: "fr",
       owner: "U1",
       isComplete: true,
     },
     {
       name: "Production optimizations",
-      id: "T5",
-      group: "G3",
+      id: "P5",
+      language: "fr",
       owner: "U1",
       isComplete: false,
     },
-  ],
-  comments: [
     {
+      name: "Production optimizations",
+      id: "P6",
+      language: "fr",
       owner: "U1",
-      id: "C1",
-      task: "T1",
-      content: "Great work!",
+      isComplete: false,
+    },
+    {
+      name: "Update component snapshots",
+      id: "P7",
+      language: "fr",
+      owner: "U1",
+      isComplete: true,
+    },
+    {
+      name: "Production optimizations",
+      id: "P8",
+      language: "fr",
+      owner: "U1",
+      isComplete: false,
+    },
+    {
+      name: "Production optimizations",
+      id: "P9",
+      language: "fr",
+      owner: "U1",
+      isComplete: false,
     },
   ],
   languages: [
